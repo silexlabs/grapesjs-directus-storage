b56c8f848c6b0c3160dd210c07421083e3bb57be
Alex Hoyau <alex@lexoyo.me>
2023-03-27 16:36:59 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import initDirectus from './directus';
+import en from './locale/en';
+
+export default (editor, opts = {}) => {
+  const options = { ...{
+    i18n: {},
+    // default options
+  },  ...opts };
+
+  initDirectus(editor, options);
+
+  // Load i18n files
+  editor.I18n && editor.I18n.addMessages({
+      en,
+      ...options.i18n,
+  });
+
+  // TODO Remove
+  editor.on('load', () =>
+    editor.addComponents(
+        `<div style="margin:100px; padding:25px;">
+            Content loaded from the plugin
+        </div>`,
+        { at: 0 }
+    ))
+};
---
+++ src/locale/en.js
@@ -0,0 +1,5 @@
+export default {
+    'grapesjs-directus-storage': {
+        // 'key': 'value',
+    },
+};
